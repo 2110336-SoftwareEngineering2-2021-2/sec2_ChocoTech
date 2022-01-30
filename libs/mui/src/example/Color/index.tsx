47d3bd482264ed9b4ab8e33ea6e1dab0e23de552
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2022-01-30 18:53:32 +0700

    fix emotion forward props

--- libs/mui/src/example/Color/index.tsx
+++ libs/mui/src/example/Color/index.tsx
@@ -1,13 +1,14 @@
+import isPropValid from '@emotion/is-prop-valid'
 import { styled, useTheme } from '@mui/material'
 import { get } from 'lodash'
 
-// import styled from '../../lib/styled/styled'
+const shouldForwardProp = (props: string) => isPropValid(props) && props !== 'color'
 
 export interface ColorProps {
   color: string
 }
 
-const Color = styled('div')<ColorProps>`
+const Color = styled('div', { shouldForwardProp })<ColorProps>`
   width: 40px;
   height: 40px;
   border-radius: 50%;
