81e4bb58f556eff3d465f218672f41de29924a81
PoraRee <ree_inter5@hotmail.com>
2022-02-06 21:45:33 +0700

    add responsive in mainpage

--- apps/doji-frontend/src/pages/_app.tsx
+++ apps/doji-frontend/src/pages/_app.tsx
@@ -3,15 +3,15 @@ import { ThemeProvider } from '@mui/material'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import './style.css'
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
         <title>Welcome to doji-frontend!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </ThemeProvider>
   )
 }
--- apps/doji-frontend/src/pages/index.tsx
+++ apps/doji-frontend/src/pages/index.tsx
@@ -1,42 +1,44 @@
-import { Box, Button, Stack, Typography } from '@mui/material'
+import { Box, Button, Container, Stack, Typography } from '@mui/material'
 import Link from 'next/link'
 
 export function Index() {
   return (
-    <Stack p={4} alignItems="center">
-      <Typography variant="title3" fontWeight={700} p={4} align="center">
-        Do
-        <Typography variant="title3" component="span" color="primary">
-          ji
-        </Typography>
-      </Typography>
-      <Box component="img" alt="" src="/static/AL.png" />
-      <Typography variant="title3" fontWeight={700} pt={4} lineHeight="32px">
-        Your{' '}
-        <Typography variant="title3" component="span" color="primary" lineHeight="32px">
-          Crypto
-        </Typography>
-      </Typography>
-      <Typography variant="title3" fontWeight={700} pb={11} lineHeight="32px">
-        Trading{' '}
-        <Typography variant="title3" component="span" color="primary" lineHeight="32px">
-          Assistant
-        </Typography>
-      </Typography>
-      <div>
-        <Link href="/register" passHref>
-          <Button>Create Account</Button>
-        </Link>
-      </div>
-      <Typography variant="regular" pt={3}>
-        Have an account?{' '}
-        <Link href="/login" passHref>
-          <Typography variant="regular" component="a" color="primary">
-            Log in
+    <Container maxWidth="sm">
+      <Stack direction="column" justifyContent="space-between" alignItems="center" mt={4}>
+        <Typography variant="title3" fontWeight={700} p={[2, 4]} align="center">
+          Do
+          <Typography variant="title3" component="span" color="primary">
+            ji
           </Typography>
-        </Link>
-      </Typography>
-    </Stack>
+        </Typography>
+        <Box component="img" alt="" src="/static/AL.png" />
+        <Typography variant="title3" fontWeight={700} pt={[2, 4]} lineHeight="32px">
+          Your{' '}
+          <Typography variant="title3" component="span" color="primary" lineHeight="32px">
+            Crypto
+          </Typography>
+        </Typography>
+        <Typography variant="title3" fontWeight={700} pb={[2, 11]} lineHeight="32px">
+          Trading{' '}
+          <Typography variant="title3" component="span" color="primary" lineHeight="32px">
+            Assistant
+          </Typography>
+        </Typography>
+        <div>
+          <Link href="/register" passHref>
+            <Button>Create Account</Button>
+          </Link>
+        </div>
+        <Typography variant="regular" pt={3}>
+          Have an account?{' '}
+          <Link href="/login" passHref>
+            <Typography variant="regular" component="a" color="primary">
+              Log in
+            </Typography>
+          </Link>
+        </Typography>
+      </Stack>
+    </Container>
   )
 }
 
