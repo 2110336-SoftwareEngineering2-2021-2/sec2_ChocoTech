c6ccafe2022d304aada21198d24bb5dbc8958a58
PoraRee <ree_inter5@hotmail.com>
2022-02-06 20:51:10 +0700

    delete old style.css and edit app.tsx

--- apps/doji-frontend/src/pages/_app.tsx
+++ apps/doji-frontend/src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { ThemeProvider } from '@mui/material'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import './styles.css'
-
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
