1acf2c557bcbd00ecdbdea0c326fb9e7a7ef2df4
PoraRee <ree_inter5@hotmail.com>
2022-02-13 01:43:45 +0700

    fixed responsive

--- apps/doji-frontend/src/pages/index.tsx
+++ apps/doji-frontend/src/pages/index.tsx
@@ -1,15 +1,23 @@
-import { Button, Container, Stack, Typography, styled } from '@mui/material'
+import { Button, Container, Link as MuiLink, Stack, Typography, styled } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const StyledContainer = styled(Container)({
-  minHeight: '100%',
-})
-
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-grow: 1;
+  min-height: 600px;
+  margin-top: ${({ theme }) => theme.spacing(4)};
+`
 export function Index() {
   return (
     <StyledContainer maxWidth="sm">
-      <Stack direction="column" justifyContent="space-between" alignItems="center" pt={4}>
+      <Stack
+        sx={{ minHeight: '100%' }}
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        flexGrow={1}
+      >
         <Typography variant="title3" fontWeight={700} p={[2, 4]} align="center">
           Do
           <Typography variant="title3" component="span" color="primary">
@@ -17,31 +25,39 @@ export function Index() {
           </Typography>
         </Typography>
         <Image src="/static/AL.png" alt="Picture of the author" width={327} height={327}></Image>
-        <Typography variant="title3" fontWeight={700} pt={[2, 4]} lineHeight="32px">
-          Your{' '}
-          <Typography variant="title3" component="span" color="primary" lineHeight="32px">
-            Crypto
+        <Stack direction="column">
+          <Typography
+            align="center"
+            variant="title3"
+            fontWeight={700}
+            pt={[2, 4]}
+            lineHeight="32px"
+          >
+            Your{' '}
+            <Typography variant="title3" component="span" color="primary" lineHeight="32px">
+              Crypto
+            </Typography>
           </Typography>
-        </Typography>
-        <Typography variant="title3" fontWeight={700} pb={[2, 11]} lineHeight="32px">
-          Trading{' '}
-          <Typography variant="title3" component="span" color="primary" lineHeight="32px">
-            Assistant
+          <Typography variant="title3" fontWeight={700} pb={[2, 11]} lineHeight="32px">
+            Trading{' '}
+            <Typography variant="title3" component="span" color="primary" lineHeight="32px">
+              Assistant
+            </Typography>
           </Typography>
-        </Typography>
-        <div>
+        </Stack>
+        <Stack direction="column" spacing={2} mb={5}>
           <Link href="/register" passHref>
             <Button>Create Account</Button>
           </Link>
-        </div>
-        <Typography variant="regular" pt={3}>
-          Have an account?{' '}
-          <Link href="/login" passHref>
-            <Typography variant="regular" component="a" color="primary">
-              Log in
-            </Typography>
-          </Link>
-        </Typography>
+          <Typography variant="regular">
+            Have an account?{' '}
+            <Link href="/login" passHref>
+              <MuiLink variant="regular" color="primary">
+                Log in
+              </MuiLink>
+            </Link>
+          </Typography>
+        </Stack>
       </Stack>
     </StyledContainer>
   )
