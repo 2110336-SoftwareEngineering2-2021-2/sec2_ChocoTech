4775c9c9b52d4688f564eab5881e129a732b3165
Pipat Saengow <pay2630@gmail.com>
2022-02-14 02:33:00 +0700

    backend: fix regression in Redis provider due to missing return

--- apps/doji-backend/src/external/external.module.ts
+++ apps/doji-backend/src/external/external.module.ts
@@ -2,20 +2,27 @@ import { Logger, Module } from '@nestjs/common'
 import Redis from 'ioredis'
 import { environment } from 'src/environments/environment'
 
+export function newRedisClient() {
+  const redis = new Redis({
+    host: environment.redis.host,
+    db: environment.redis.db,
+  })
+  const logger = new Logger('Redis')
+  redis.on('error', (err) => logger.error(err))
+  return redis
+}
+
 @Module({
   providers: [
     {
       provide: 'Redis',
-      useFactory: () => {
-        const redis = new Redis({
-          host: environment.redis.host,
-          db: environment.redis.db,
-        })
-        const logger = new Logger('Redis')
-        redis.on('error', (err) => logger.error(err))
-      },
+      useFactory: newRedisClient,
+    },
+    {
+      provide: 'RedisSubscriber',
+      useFactory: newRedisClient,
     },
   ],
-  exports: ['Redis'],
+  exports: ['Redis', 'RedisSubscriber'],
 })
 export class ExternalModule {}
