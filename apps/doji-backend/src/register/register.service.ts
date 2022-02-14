793e42764ee9f2b46798634f2485e29fc21ff782
PoraRee <ree_inter5@hotmail.com>
2022-02-14 22:52:07 +0700

    wip

--- apps/doji-backend/src/register/register.service.ts
+++ apps/doji-backend/src/register/register.service.ts
@@ -1,9 +1,11 @@
 import { EntityRepository, UniqueConstraintViolationException } from '@mikro-orm/core'
 import { InjectRepository } from '@mikro-orm/nestjs'
+import { colors } from '@mui/material'
 import { HttpException, HttpStatus, Injectable, UnprocessableEntityException } from '@nestjs/common'
 import bcrypt from 'bcrypt'
+import { UserReference } from 'src/auth/auth.service'
 import { User } from 'src/entities/User'
-import { UserRegistrationRequest } from 'src/register/register.dto'
+import { UserChangePassword, UserRegistrationRequest } from 'src/register/register.dto'
 
 @Injectable()
 export class RegisterService {
@@ -16,14 +18,21 @@ export class RegisterService {
     newUser.displayName = dto.displayName
     newUser.passwordHash = await bcrypt.hash(dto.password, 10)
     try {
-      await this.userRepo.persistAndFlush(newUser)//add to database
+      await this.userRepo.persistAndFlush(newUser) //add to database
     } catch (e) {
       if (e instanceof UniqueConstraintViolationException) {
-        throw new UnprocessableEntityException("User with this username or email already exist.")
+        throw new UnprocessableEntityException('User with this username or email already exist.')
       } else {
         throw e
       }
     }
     return
+  }
+  async changePassword(dto: UserChangePassword, userRef: UserReference) {
+    const user = await userRef.getUser()
+    if (user.passwordHash == (await bcrypt.hash(dto.currentPassword, 10))) {
+      user.passwordHash = await bcrypt.hash(dto.newPassword, 10)
+    }
+    await this.userRepo.persistAndFlush(user)
   }
 }
