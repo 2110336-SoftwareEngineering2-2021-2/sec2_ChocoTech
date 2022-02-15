438c99e6a5d93a136fa1fa83b62d5e102ce6f197
Thanapoom02281 <70056574+Thanapoom02281@users.noreply.github.com>
2022-02-15 22:58:51 +0700

    change file name

--- apps/doji-frontend/src/components/ChangePassword/ChangePasswordBar.tsx
+++ apps/doji-frontend/src/components/ChangePassword/ChangePasswordBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Grid, IconButton, Toolbar, Typography, styled } from '@mui/material'
+import { AppBar, Grid, IconButton, Stack, Toolbar, Typography, styled } from '@mui/material'
 
 import { IoIosArrowBack } from 'react-icons/io'
 
@@ -16,20 +16,14 @@ export function ChangePasswordBar() {
         color: 'black',
       }}
     >
-      <Toolbar>
-        <Grid container spacing={0} sx={{ flexGrow: 1, alignItems: 'center' }}>
-          <Grid xs={1} item>
-            <IconButton edge="start" aria-label="menu">
-              <IoIosArrowBack></IoIosArrowBack>
-            </IconButton>
-          </Grid>
-          <Grid xs={11} textAlign="center" item>
-            <Typography variant="large" fontWeight="400">
-              Change Password
-            </Typography>
-          </Grid>
-        </Grid>
-      </Toolbar>
+      <Stack direction="row">
+        <IconButton edge="start" aria-label="menu" size="large" sx={{ margin: '5px' }}>
+          <IoIosArrowBack></IoIosArrowBack>
+        </IconButton>
+        <Typography variant="large" fontWeight="400" margin="auto">
+          Change Password
+        </Typography>
+      </Stack>
     </StyledAppBar>
   )
 }
