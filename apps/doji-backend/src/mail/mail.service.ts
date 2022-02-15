20cbc4c96a1fba28dee2f24e64ff3576725f5172
Phumpakorns <66027264+Phumpakorns@users.noreply.github.com>
2022-02-15 14:40:24 +0700

    Add mail service

--- apps/doji-backend/src/mail/mail.service.ts
+++ apps/doji-backend/src/mail/mail.service.ts
@@ -1,4 +1,22 @@
+import { MailerService } from '@nestjs-modules/mailer'
 import { Injectable } from '@nestjs/common'
+import { User } from 'src/entities/User'
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendPasswordReset(user: User, token: String) {
+    const url = 'path-to-reset-page?token=${token}'
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      subject: 'Reset password for your Doji account',
+      template: './passwordreset',
+      context: {
+        name: user.username,
+        url,
+      },
+    })
+  }
+}
