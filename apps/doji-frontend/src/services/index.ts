bf529311738b9a459424347d822c6f49ca776a8b
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2022-02-20 20:22:03 +0700

    add axios interceptors

--- apps/doji-frontend/src/services/index.ts
+++ apps/doji-frontend/src/services/index.ts
@@ -1,6 +1,17 @@
+import Storage from '@frontend/common/storage'
+import { StorageKey } from '@frontend/common/storage/constants'
 import axios from 'axios'
 
 export const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
+})
+
+httpClient.interceptors.request.use((request) => {
+  const token = new Storage('localStorage').get(StorageKey.TOKEN)
+  if (token) {
+    console.log('Add Header Authorization')
+    request.headers.Authorization = `Bearer ${token}`
+  }
+  return request
 })
