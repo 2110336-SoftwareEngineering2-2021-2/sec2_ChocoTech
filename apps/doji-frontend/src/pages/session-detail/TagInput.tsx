009a8668a03ab6ea586bb278dea9639137ee9b9a
Thanapoom <6230228121@student.chula.ac.th>
2022-02-24 15:23:27 +0700

    add form inputs

---
+++ apps/doji-frontend/src/pages/session-detail/TagInput.tsx
@@ -0,0 +1,50 @@
+import { Tables } from '@libs/mui'
+import { Autocomplete, Avatar, Chip, ListItem, TextField } from '@mui/material'
+
+const friendList = [
+  {
+    value: '1',
+    text: 'Mr.Incredible',
+  },
+  {
+    value: '2',
+    text: 'Yeltsa kciR',
+  },
+  {
+    value: '3',
+    text: 'Super Idol',
+  },
+  {
+    value: '4',
+    text: 'Ricardo',
+  },
+]
+export default function TagsInput() {
+  return (
+    <div>
+      <Autocomplete
+        filterSelectedOptions={true}
+        multiple
+        options={friendList}
+        getOptionLabel={(option) => option.text}
+        key={(option) => option.value}
+        renderTags={(value, getTagProps) =>
+          value.map((option, index) => (
+            <Chip
+              variant="outlined"
+              label={option.text}
+              avatar={<Avatar></Avatar>}
+              {...getTagProps({ index })}
+            ></Chip>
+          ))
+        }
+        renderInput={(params) => <TextField {...params}></TextField>}
+        renderOption={(props, option, { selected }) => (
+          <li {...props} id={option.value}>
+            <Tables content={option.text} avatar={<Avatar></Avatar>}></Tables>
+          </li>
+        )}
+      ></Autocomplete>
+    </div>
+  )
+}
--- apps/doji-frontend/src/pages/session-detail/index.tsx
+++ apps/doji-frontend/src/pages/session-detail/index.tsx
@@ -1,21 +1,34 @@
 import { SearchBar, Tables, TopBar } from '@libs/mui'
-import { Avatar, Box, Grid, Typography } from '@mui/material'
+import { DatePicker, TimePicker } from '@mui/lab'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { Avatar, Box, Grid, TextField, Typography } from '@mui/material'
+
+import React from 'react'
+
+import TagsInput from './TagInput'
 
 export function Index() {
+  const [date, setDate] = React.useState<Date | null>(null)
+  const [startTime, setStartTime] = React.useState<Date | null>(null)
+  const [endTime, setEndTime] = React.useState<Date | null>(null)
+  function selectedTagsHandler(items) {
+    console.log(items)
+  }
   return (
     <Box>
       <TopBar title="New session" action="back"></TopBar>
       <Box>
-        <Grid container alignItems="center">
+        <Grid container alignItems="center" display="flex">
           <Grid item xs={12}>
             <Typography fontWeight={700} variant="title3">
               How to read indicators
             </Typography>
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={8} marginBottom={2}>
             <Tables content="by Rick Astley" avatar={<Avatar></Avatar>}></Tables>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={4} marginBottom={2} textAlign="right">
             <Typography variant="large" fontWeight={700} color="#367D7F">
               250
             </Typography>
@@ -23,14 +36,55 @@ export function Index() {
               /hr/person
             </Typography>
           </Grid>
-          <Grid item xs={12} marginTop={2} marginBottom={2}>
+          <Grid item xs={12} marginBottom={3}>
             <Typography variant="regular" fontWeight={400}>
               asdjflskd aslkdfl; k;laskd ksljdklf masm,ndfl kskl jlkjlks kasjdljfkldj lkasjdflksn
               m,id asd ,m ns,m kjsdlfkja m,.ma sdkfj lk
             </Typography>
           </Grid>
-          <Grid item xs={12} marginTop={2} marginBottom={2}>
-            <Date></Date>
+          <Grid item xs={12} marginBottom={3}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Date"
+                value={date}
+                onChange={(newDate) => {
+                  setDate(newDate)
+                }}
+                renderInput={(params) => <TextField {...params} fullWidth />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={6} marginBottom={3}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <TimePicker
+                label="Start Time"
+                value={startTime}
+                onChange={(newTime) => {
+                  setStartTime(newTime)
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={6} marginBottom={3}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <TimePicker
+                label="End Time"
+                value={endTime}
+                onChange={(newTime) => {
+                  setEndTime(newTime)
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12} marginBottom={2}>
+            <Typography variant="large" fontWeight={700}>
+              Participants
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TagsInput />
           </Grid>
         </Grid>
       </Box>
