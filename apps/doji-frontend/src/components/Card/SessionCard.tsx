3f4ac7f009e19c018ce9ecb2118b7d48c75ffb59
Uttanon <uttanon.aug@gmail.com>
2022-02-26 23:20:18 +0700

    Change from literal styled to props styled

--- apps/doji-frontend/src/components/Card/SessionCard.tsx
+++ apps/doji-frontend/src/components/Card/SessionCard.tsx
@@ -1,70 +1,11 @@
-import { Avatar, Container, Typography, styled } from '@mui/material'
+import { Avatar, Divider, Stack, Typography, styled } from '@mui/material'
 import { Box } from '@mui/system'
 
-const MainFlex = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  padding-top: ${({ theme }) => theme.spacing(2)};
-  padding-bottom: ${({ theme }) => theme.spacing(2)};
-  border-bottom: 1px solid;
-  border-color: ${({ theme }) => theme.palette.sky.lighter};
-`
-const HeaderFlex = styled(Box)`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: space-between;
-`
-const ProfileNameFlex = styled(Box)`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-end;
-`
-const PriceFlex = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`
-const TypoPrice = styled(Typography)`
-  color: ${({ theme }) => theme.palette.primary.dark};
-  font-weight: ${({ theme }) => theme.typography.fontWeightBold};
-  font-size: large;
-`
-const TypoPriceSuffix = styled(Typography)`
-  color: ${({ theme }) => theme.palette.primary.dark};
-  font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-  font-size: medium;
-`
-const TypoStyleTopic = styled(Typography)`
-  font-weight: ${({ theme }) => theme.typography.fontWeightBold};
-  font-size: large;
-`
-const TypoNamePrefix = styled(Typography)`
-  margin-left: ${({ theme }) => theme.spacing(1)};
-  margin-right: ${({ theme }) => theme.spacing(0.5)};
-  font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
-  color: ${({ theme }) => theme.palette.ink.lighter};
-  display: inline;
-  font-size: medium;
-`
-const TypoName = styled(Typography)`
-  font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-  color: ${({ theme }) => theme.palette.ink.lighter};
-  display: inline;
-  font-size: medium;
-`
-const DetailStyle = styled(Typography)`
-  color: ${({ theme }) => theme.palette.ink.main};
-  margin-top: ${({ theme }) => theme.spacing(1)};
-  display: inline;
-  font-size: medium;
-`
 const StyleAvatar = styled(Avatar)`
   width: ${({ theme }) => theme.spacing(3)};
   height: ${({ theme }) => theme.spacing(3)};
-  margin-top: ${({ theme }) => theme.spacing(1)};
 `
-type SessionCardProps = {
+export type SessionCardProps = {
   topic: string
   expertName: string
   price: number
@@ -73,23 +14,35 @@ type SessionCardProps = {
 }
 function SessionCard(props: SessionCardProps) {
   return (
-    <MainFlex>
-      <HeaderFlex>
+    <Stack py={2} spacing={1.5}>
+      <Stack direction="row" justifyContent="space-between">
         <Box>
-          <TypoStyleTopic>{props.topic}</TypoStyleTopic>
-          <ProfileNameFlex>
+          <Typography variant="large" fontWeight={700}>
+            {props.topic}
+          </Typography>
+          <Stack direction="row" alignItems="center" mt={1}>
             <StyleAvatar src={props.imgURL} />
-            <TypoNamePrefix>by</TypoNamePrefix>
-            <TypoName>{props.expertName}</TypoName>
-          </ProfileNameFlex>
+            <Typography color="ink.lighter" ml={1} mr={0.5} variant="small" fontWeight={400}>
+              by
+            </Typography>
+            <Typography color="ink.lighter" variant="small" fontWeight={500}>
+              {props.expertName}
+            </Typography>
+          </Stack>
         </Box>
-        <PriceFlex>
-          <TypoPrice>{props.price}</TypoPrice>
-          <TypoPriceSuffix>/hr/person</TypoPriceSuffix>
-        </PriceFlex>
-      </HeaderFlex>
-      <DetailStyle>{props.sessionDetail}</DetailStyle>
-    </MainFlex>
+        <Stack alignItems="flex-end">
+          <Typography color="primary.dark" variant="large" fontWeight={700}>
+            {props.price}
+          </Typography>
+          <Typography color="primary.dark" variant="tiny" fontWeight={400}>
+            /hr/person
+          </Typography>
+        </Stack>
+      </Stack>
+      <Typography color="ink.main" variant="small" fontWeight={400}>
+        {props.sessionDetail}
+      </Typography>
+    </Stack>
   )
 }
 export default SessionCard
