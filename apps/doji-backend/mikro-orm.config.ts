1620f11d32f7be520bc48e6c66c4127a83d285c3
Saenyakorn Siangsanoh <33742791+saenyakorn@users.noreply.github.com>
2022-02-26 19:34:16 +0700

    fix: migrate fail (#52)

--- apps/doji-backend/mikro-orm.config.ts
+++ apps/doji-backend/mikro-orm.config.ts
@@ -2,7 +2,7 @@ import { environment } from './src/environments/environment'
 
 export default {
   type: 'postgresql',
-  entities: ['./src/entities', '../../libs/api/src/lib/entities'],
-  entitiesTs: ['./src/entities', '../../libs/api/src/lib/entities'],
+  entities: ['./src/entities'],
+  entitiesTs: ['./src/entities'],
   ...environment.database,
 }
