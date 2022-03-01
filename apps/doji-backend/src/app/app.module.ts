2f4b9032e308fd7b33774690c8c08a010ecc5e18
Thanat <thanat.phr@gmail.com>
2022-03-01 16:09:02 +0700

    Rename to session

--- apps/doji-backend/src/app/app.module.ts
+++ apps/doji-backend/src/app/app.module.ts
@@ -36,7 +36,7 @@ import { AppService } from './app.service'
     RegisterModule,
     ProfileModule,
     MessagingModule,
-    ScheduleModule,
+    SessionModule,
     PaymentModule,
     WorkHistoryModule,
     SessionModule,
