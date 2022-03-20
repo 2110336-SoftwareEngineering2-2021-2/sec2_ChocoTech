c8e6a482dcf3887c5cd6472385db6f2bb56a55b3
Thanat <thanat.phr@gmail.com>
2022-03-20 09:46:38 +0700

    Fix POST review

--- apps/doji-backend/src/review/review.service.ts
+++ apps/doji-backend/src/review/review.service.ts
@@ -1,4 +1,5 @@
 import { Review } from '@backend/entities/Review'
+import { Session } from '@backend/entities/Session'
 import { ReviewCreationRequestDTO } from '@backend/review/review.dto'
 import { IUserReference } from '@libs/api'
 import { EntityRepository, UniqueConstraintViolationException } from '@mikro-orm/core'
@@ -7,15 +8,22 @@ import { Injectable, UnprocessableEntityException } from '@nestjs/common'
 
 @Injectable()
 export class ReviewService {
-  constructor(@InjectRepository(Review) private readonly reviewRepo: EntityRepository<Review>) {}
+  constructor(
+    @InjectRepository(Review) private readonly reviewRepo: EntityRepository<Review>,
+    @InjectRepository(Session) private readonly sessionRepo: EntityRepository<Session>,
+  ) {}
 
   async createReview(dto: ReviewCreationRequestDTO, userRef: IUserReference) {
     const user = await userRef.getUser()
+    const session = await this.sessionRepo.findOne({ id: dto.sessionId })
+    if (!session) return null
     const newReview = new Review()
+    newReview.user = user
+    newReview.session = session
     newReview.content = dto.content
     newReview.rating = dto.rating
     try {
-      await this.reviewRepo.persistAndFlush(newReview) //add to database
+      await this.reviewRepo.persistAndFlush(newReview)
     } catch (e) {
       if (e instanceof UniqueConstraintViolationException) {
         throw new UnprocessableEntityException('User with this username or email already exist.')
