752eeada04bb179860fe32b87b2192c1acfddf1e
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-03-24 17:12:22 +0700

    wip

---
+++ apps/doji-frontend/src/modules/settings/index.tsx
@@ -0,0 +1,76 @@
+import NotiDialog from '@frontend/components/NotiDialog/NotiDialog'
+import { ExtendedNextPage } from '@frontend/type'
+import { IMeResponseDTO } from '@libs/api'
+import { CompactPrpfile } from '@libs/mui'
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Tab,
+  Tabs,
+  Typography,
+} from '@mui/material'
+import Link from 'next/link'
+
+import { ReactNode, useMemo } from 'react'
+import { FiDollarSign, FiEdit2, FiLayers, FiLock, FiLogOut } from 'react-icons/fi'
+
+import { ExpertCard, ExpertCardBody, ExpertCardHeader } from './styled'
+
+interface ListItemProps {
+  href: string
+  text: string
+  icon: ReactNode
+}
+
+const ListItem: React.FC<ListItemProps> = ({ href, text, icon }) => {
+  return (
+    <Link href={href} passHref>
+      <ListItemButton>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItemButton>
+    </Link>
+  )
+}
+
+interface SettingsPageProps {
+  user: IMeResponseDTO
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = ({ user }) => {
+  const displayName = useMemo(() => {
+    if (user.displayName) return user.displayName
+    if (user.firstName && user.lastName) return `${user.firstName} ${user.lastName}`
+    return user.username
+  }, [user])
+
+  return (
+    <Stack direction="column">
+      <Typography variant="title3" py={2} px={3}>
+        Settings
+      </Typography>
+      <CompactPrpfile
+        username={user.username}
+        displayName={displayName}
+        profileUrl={user.profilePictureURL}
+      />
+      <ExpertCard>
+        <ExpertCardHeader>Become expert</ExpertCardHeader>
+        <ExpertCardBody>To create your own sessions and make an income</ExpertCardBody>
+      </ExpertCard>
+      <List component="nav">
+        <ListItem href="/settings/profile" text="Edit Profiel" icon={<FiEdit2 />} />
+        <ListItem href="/settings/change-password" text="Change password" icon={<FiLock />} />
+        <ListItem href="/settings/wallet" text="Wallet" icon={<FiDollarSign />} />
+        <ListItem href="/settings/experience" text="Experience" icon={<FiLayers />} />
+        <ListItem href="/logout" text="Logout" icon={<FiLogOut />} />
+      </List>
+    </Stack>
+  )
+}
+
+export default SettingsPage
---
+++ apps/doji-frontend/src/modules/settings/styled.tsx
@@ -0,0 +1,23 @@
+import { Typography, styled } from '@mui/material'
+
+export const ExpertCard = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing(1)};
+  padding: ${({ theme }) => theme.spacing(2.5, 2)};
+  background: ${({ theme }) => theme.palette.primary.main};
+`
+
+export const ExpertCardHeader = styled(Typography)``
+ExpertCardHeader.arguments = {
+  variant: 'large',
+  fontWeight: 700,
+  color: 'white',
+}
+
+export const ExpertCardBody = styled(Typography)``
+ExpertCardHeader.arguments = {
+  variant: 'small',
+  fontWeight: 400,
+  color: 'white',
+}
