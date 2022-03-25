16e6b3e777ca8b406e6c6aeb4473c8297dae5704
PoraRee <ree_inter5@hotmail.com>
2022-03-25 23:43:28 +0700

    Edit flex in Achievement component (to manage menu)

--- libs/mui/src/lib/Achievement/index.tsx
+++ libs/mui/src/lib/Achievement/index.tsx
@@ -1,4 +1,4 @@
-import { Dialog as MuiDialog, Stack, Typography, styled } from '@mui/material'
+import { Button, Dialog, Stack, Typography, styled } from '@mui/material'
 import Image from 'next/image'
 
 import React from 'react'
@@ -9,10 +9,6 @@ export interface AchievementProps {
   src?: string
   editable?: boolean
 }
-const Dialog = styled(MuiDialog)`
-  width: '100%';
-  height: '100vh';
-`
 const ImgFrame = styled(Image)`
   object-fit: contain;
 `
@@ -28,19 +24,31 @@ export const Achievement: React.FC<AchievementProps> = ({ title, desc, src, edit
     setOpen(false)
   }
   return (
-    <Stack direction="row" spacing={2} py={2} px={3} mb={2}>
-      <StyledImg src={src} width={102} height={102} onClick={handleOpen} />
-      <Dialog onClose={handleClose} open={open} fullScreen>
-        <ImgFrame src={src} layout="fill" onClick={handleClose} />
-      </Dialog>
-      <Stack direction="column" spacing={1} justifyContent="flex-start">
-        <Typography variant="regular" fontWeight={700} color="ink.darkest">
-          {title}
-        </Typography>
-        <Typography variant="tiny" fontWeight={400} color="ink.main">
-          {desc}
-        </Typography>
+    <Stack
+      direction="row"
+      spacing={1}
+      py={2}
+      px={3}
+      mb={2}
+      sx={{ width: '100%', boxSizing: 'border-box' }}
+      justifyContent="space-between"
+    >
+      <Stack direction="row" spacing={2}>
+        <StyledImg src={src} width={102} height={102} onClick={handleOpen} />
+        <Dialog onClose={handleClose} open={open} fullScreen>
+          <ImgFrame src={src} layout="fill" onClick={handleClose} />
+        </Dialog>
+
+        <Stack direction="column" spacing={1} justifyContent="flex-start">
+          <Typography variant="regular" fontWeight={700} color="ink.darkest">
+            {title}
+          </Typography>
+          <Typography variant="tiny" fontWeight={400} color="ink.main">
+            {desc}
+          </Typography>
+        </Stack>
       </Stack>
+      <Button>tmp</Button>
     </Stack>
   )
 }
