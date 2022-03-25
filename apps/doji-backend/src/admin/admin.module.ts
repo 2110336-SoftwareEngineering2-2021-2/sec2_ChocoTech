71ed33f3ea556c529a6753b860dd1feec16b2e21
Thanat <thanat.phr@gmail.com>
2022-03-25 21:28:43 +0700

    Add Get approve detail and Put userRole to expert

--- apps/doji-backend/src/admin/admin.module.ts
+++ apps/doji-backend/src/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { AdminController } from '@backend/admin/admin.controller'
 import { AdminService } from '@backend/admin/admin.service'
 import { Admin } from '@backend/entities/Admin'
+import { User } from '@backend/entities/User'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Admin])],
+  imports: [MikroOrmModule.forFeature([Admin, User])],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
