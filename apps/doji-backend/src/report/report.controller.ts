7ece3f80bfea4d97273fd0b6fc92008a24a425f5
Thanapoom <6230228121@student.chula.ac.th>
2022-03-28 10:15:01 +0700

    Implement report expert

---
+++ apps/doji-backend/src/report/report.controller.ts
@@ -0,0 +1,17 @@
+import { CurrentUser, UserAuthGuard } from '@backend/auth/user.guard'
+import { User } from '@backend/entities/User'
+import { ReportDTO } from '@backend/report/report.dto'
+import { ReportService } from '@backend/report/report.service'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { ApiCookieAuth } from '@nestjs/swagger'
+
+@Controller('report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+  @Post()
+  @UseGuards(UserAuthGuard)
+  @ApiCookieAuth()
+  async report(@Body() dto: ReportDTO, @CurrentUser() user) {
+    return await this.reportService.report(dto, user)
+  }
+}
