9fed646c5daea42dbd4400070fbc826940423d3d
phumpakorn s <phumpakorns@gmail.com>
2022-03-29 21:31:32 +0700

    added interface IFriendRequest

---
+++ libs/api/src/lib/entities/FriendRequest.ts
@@ -0,0 +1,11 @@
+import { friendRequestStatus } from 'libs/api/src/lib/constants/friendRequestStatus'
+import { IUser } from 'libs/api/src/lib/entities/User'
+
+export interface IFriendRequest {
+  id: string
+  sender: IUser
+  dateSent: Date
+  receiver: IUser
+  dateResponse: Date
+  status: friendRequestStatus
+}
