b25b70e12f20ef9620f5fd1282495b032a574e71
Uttanon <uttanon.aug@gmail.com>
2022-03-30 22:05:07 +0700

    wip

--- apps/doji-frontend/src/pages/session/index.tsx
+++ apps/doji-frontend/src/pages/session/index.tsx
@@ -1,31 +1,38 @@
+import { httpClient } from '@frontend/services'
 import { ExtendedNextPage } from '@frontend/type'
+import { ISchedule } from '@libs/api'
 import { SearchBar, SessionCard, SessionCardProps } from '@libs/mui'
 import { Divider, Stack } from '@mui/material'
 
-const mockDatas: SessionCardProps[] = [
-  {
-    topic: 'hello',
-    expertName: 'Poravee',
-    price: 250,
-    profileImageURL: 'https://mui.com/static/images/avatar/1.jpg',
-    sessionDetail: 'OS Session',
-  },
-  {
-    topic: 'Poravee2',
-    expertName: 'Poravee',
-    price: 875,
-    profileImageURL: 'https://mui.com/static/images/avatar/1.jpg',
-    sessionDetail: 'SE MOCKUP DATA',
-  },
-]
+import { useQuery } from 'react-query'
+
+// const mockDatas: SessionCardProps[] = [
+//   {
+//     topic: 'hello',
+//     expertName: 'Poravee',
+//     price: 250,
+//     profileImageURL: 'https://mui.com/static/images/avatar/1.jpg',
+//     sessionDetail: 'OS Session',
+//   },
+//   {
+//     topic: 'Poravee2',
+//     expertName: 'Poravee',
+//     price: 875,
+//     profileImageURL: 'https://mui.com/static/images/avatar/1.jpg',
+//     sessionDetail: 'SE MOCKUP DATA',
+//   },
+// ]
 
 const Index: ExtendedNextPage = () => {
+  const { data, isLoading } = useQuery<ISchedule[]>('/session', () =>
+    httpClient.get('/session').then((res) => res.data),
+  )
   return (
     <>
       <SearchBar />
       <Stack divider={<Divider flexItem />} spacing={2}>
-        {mockDatas.map((data, index) => (
-          <SessionCard {...data} key={index} />
+        {data.map((elem, index) => (
+          <SessionCard {...elem} key={index} />
         ))}
       </Stack>
     </>
--- libs/mui/src/lib/SessionCard/index.tsx
+++ libs/mui/src/lib/SessionCard/index.tsx
@@ -1,37 +1,39 @@
+import { ISchedule, ISession } from '@libs/api'
 import { Avatar, Stack, Typography, styled } from '@mui/material'
 
 const StyleAvatar = styled(Avatar)`
   width: ${({ theme }) => theme.spacing(3)};
   height: ${({ theme }) => theme.spacing(3)};
 `
-export type SessionCardProps = {
-  topic: string
-  expertName: string
-  price: number
-  profileImageURL: string
-  sessionDetail?: string
-}
+// export type SessionCardProps = {
+//   topic: string
+//   expertName: string
+//   price: number
+//   profileImageURL: string
+//   sessionDetail?: string
+// }
+export interface SessionCardProps extends ISchedule {}
 export function SessionCard(props: SessionCardProps) {
   return (
     <Stack py={2} spacing={1.5}>
       <Stack direction="row" justifyContent="space-between">
         <div>
           <Typography variant="large" fontWeight={700}>
-            {props.topic}
+            {props.session.topic}
           </Typography>
           <Stack direction="row" alignItems="center" mt={1}>
-            <StyleAvatar src={props.profileImageURL} />
+            <StyleAvatar src={props.creator.profilePictureURL} />
             <Typography color="ink.lighter" ml={1} mr={0.5} variant="small" fontWeight={400}>
               by
             </Typography>
             <Typography color="ink.lighter" variant="small" fontWeight={500}>
-              {props.expertName}
+              {props.creator.username}
             </Typography>
           </Stack>
         </div>
         <Stack alignItems="flex-end">
           <Typography color="primary.dark" variant="large" fontWeight={700}>
-            {`${props.price} DC`}
+            {`${props.session.fee} DC`}
           </Typography>
           <Typography color="primary.dark" variant="tiny" fontWeight={400}>
             /hr/person
@@ -39,7 +41,7 @@ export function SessionCard(props: SessionCardProps) {
         </Stack>
       </Stack>
       <Typography color="ink.main" variant="small" fontWeight={400}>
-        {props.sessionDetail}
+        {props.session.description}
       </Typography>
     </Stack>
   )
