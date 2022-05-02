52fdacd419c1b608f1c7e7e170487ae3750007c7
phumpakorn s <phumpakorns@gmail.com>
2022-05-02 22:50:00 +0700

    insert getfriend path, added page

---
+++ apps/doji-frontend/src/pages/friend/find/index.tsx
@@ -0,0 +1,5 @@
+import { ExtendedNextPage } from '@frontend/type'
+
+const FindFriendPage: ExtendedNextPage = () => {
+  return <h1>This should show the list of user you can add as friend</h1>
+}
---
+++ apps/doji-frontend/src/pages/friend/index.tsx
@@ -0,0 +1,5 @@
+import { ExtendedNextPage } from '@frontend/type'
+
+const FriendPage: ExtendedNextPage = () => {
+  return <h1>This should show the list of your friend</h1>
+}
