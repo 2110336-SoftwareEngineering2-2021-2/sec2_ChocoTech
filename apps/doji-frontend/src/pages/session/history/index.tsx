fd30c7f323d256931ef0180095e9e3436d71e5cf
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-05-02 13:09:09 +0700

    fix empty session

--- apps/doji-frontend/src/pages/session/history/index.tsx
+++ apps/doji-frontend/src/pages/session/history/index.tsx
@@ -1,4 +1,5 @@
-import { Stack, Typography } from '@mui/material'
+import { Button, Stack, Typography, styled } from '@mui/material'
+import Link from 'next/link'
 import { useQuery } from 'react-query'
 
 import { httpClient } from '@frontend/services'
@@ -6,21 +7,37 @@ import { httpClient } from '@frontend/services'
 import { ISchedule } from '@libs/api'
 import { SessionHistoryCard } from '@libs/mui'
 
+const NoHistory = styled(Stack)`
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+  border: 1px solid ${({ theme }) => theme.palette.sky.light};
+`
+
 function SessionHistory() {
   const { data, isLoading } = useQuery<ISchedule[]>('/session/schedule/me', () =>
     httpClient.get('/session/schedule/me').then((res) => res.data),
   )
+
   return (
     <Stack>
-      <Typography variant="title3" py={2} mt={2}>
+      <Typography variant="title3" py={2} color="ink.dark">
         History
       </Typography>
       {!isLoading && (
         <>
-          {data.map((element, key) => {
+          {data.map((element) => {
             return <SessionHistoryCard {...element} key={element.id} />
           })}
         </>
+      )}
+      {data.length === 0 && (
+        <NoHistory p={6} alignItems="center" mt={2} gap={2}>
+          <Typography variant="large" fontWeight={500} py={2} color="sky.dark">
+            No history found
+          </Typography>
+          <Link href="/session" passHref>
+            <Button variant="outlined">Find interesting session</Button>
+          </Link>
+        </NoHistory>
       )}
     </Stack>
   )
--- apps/doji-frontend/src/pages/session/index.tsx
+++ apps/doji-frontend/src/pages/session/index.tsx
@@ -1,4 +1,4 @@
-import { Divider, Stack } from '@mui/material'
+import { Divider, Stack, Typography, styled } from '@mui/material'
 import { useRouter } from 'next/router'
 import { useQuery } from 'react-query'
 
@@ -8,6 +8,11 @@ import { ExtendedNextPage } from '@frontend/type'
 
 import { ISession } from '@libs/api'
 import { SearchBar, SessionCard } from '@libs/mui'
+
+const NoSession = styled(Stack)`
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+  border: 1px solid ${({ theme }) => theme.palette.sky.light};
+`
 
 const Index: ExtendedNextPage = () => {
   const { data, isLoading } = useQuery<ISession[]>('/session', () =>
@@ -19,9 +24,16 @@ const Index: ExtendedNextPage = () => {
     <>
       <SearchBar />
       <Stack divider={<Divider flexItem />} spacing={3} mt={4}>
-        {data.reverse().map((elem, index) => (
+        {data.reverse().map((elem) => (
           <SessionCard {...elem} key={elem.id} onClick={() => router.push(`/session/${elem.id}`)} />
         ))}
+        {data.length === 0 && (
+          <NoSession p={6} gap={2} alignItems="center">
+            <Typography variant="large" fontWeight={500} py={2} color="sky.dark">
+              No session found
+            </Typography>
+          </NoSession>
+        )}
       </Stack>
     </>
   )
