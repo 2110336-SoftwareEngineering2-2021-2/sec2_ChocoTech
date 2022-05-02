381e66e8df0aa198f9c8e492bd52f4df05551812
phumpakorn s <phumpakorns@gmail.com>
2022-05-03 03:28:19 +0700

    added friend and find friend page

--- apps/doji-frontend/src/pages/friend/find/index.tsx
+++ apps/doji-frontend/src/pages/friend/find/index.tsx
@@ -1,5 +1,65 @@
+import { Avatar, Stack, Typography } from '@mui/material'
+import StatusBadge from 'libs/mui/src/lib/StatusBadge'
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+
+import { httpClient } from '@frontend/services'
 import { ExtendedNextPage } from '@frontend/type'
 
-const FindFriendPage: ExtendedNextPage = () => {
-  return <h1>This should show the list of user you can add as friend</h1>
+import { IMinimalFriend, IUser } from '@libs/api'
+import { SearchBar } from '@libs/mui'
+
+const Index: ExtendedNextPage = () => {
+  const getCurrentUsername = async () => {
+    return await httpClient.get<IUser>('/auth/me').then((res) => res.data.username)
+  }
+  const getRelationship = async (username: string) => {
+    return await httpClient.get<string>('friend/' + username).then((res) => res.data)
+  }
+
+  const { data: users, isLoading } = useQuery('auth/users', async () => {
+    return await await httpClient.get<IUser[]>('auth/users').then((res) => res.data) //.filter(async (user) => user.username != (await getCurrentUsername()))
+    //.filter(async (user) => (await getRelationship(user.username)) !== 'friend')
+  })
+
+  const router = useRouter()
+
+  const handleUserClick = (id: string) => {
+    router.push(`/profile/${id}`)
+  }
+
+  return (
+    <Stack>
+      <Typography variant="title3" py={2} color="ink.dark">
+        Add Friends
+      </Typography>
+      <SearchBar />
+      <Stack p={2}>
+        {users
+          ? users.map((elem) => (
+              <div key={elem.username} onClick={() => handleUserClick(elem.username)}>
+                <Stack direction="row" spacing={2}>
+                  <StatusBadge username={elem.username}>
+                    <Avatar
+                      src={elem.profilePictureURL}
+                      sx={{ bgcolor: 'primary.main', width: 40, height: 40 }}
+                    >
+                      {elem.displayName.charAt(0).toUpperCase()}
+                    </Avatar>
+                  </StatusBadge>
+                  <Stack spacing={1} justifyContent="center">
+                    <Typography variant="regular">{elem.displayName}</Typography>
+                    <Typography variant="tiny" color="sky.dark">
+                      {elem.username}
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </div>
+            ))
+          : null}
+      </Stack>
+    </Stack>
+  )
 }
+
+export default Index
--- apps/doji-frontend/src/pages/friend/index.tsx
+++ apps/doji-frontend/src/pages/friend/index.tsx
@@ -1,5 +1,67 @@
+import { Avatar, Button, Stack, Typography } from '@mui/material'
+import StatusBadge from 'libs/mui/src/lib/StatusBadge'
+import { useRouter } from 'next/router'
+import { FiUserPlus } from 'react-icons/fi'
+import { useQuery } from 'react-query'
+
+import { httpClient } from '@frontend/services'
 import { ExtendedNextPage } from '@frontend/type'
 
-const FriendPage: ExtendedNextPage = () => {
-  return <h1>This should show the list of your friend</h1>
+import { IMinimalFriend } from '@libs/api'
+import { SearchBar } from '@libs/mui'
+
+const Index: ExtendedNextPage = () => {
+  const { data: Friends, isLoading } = useQuery('/friend', async () => {
+    return await httpClient.get<IMinimalFriend[]>('/friend').then((res) => res.data)
+  })
+
+  const router = useRouter()
+
+  const handleFriendClick = (id: string) => {
+    router.push(`/profile/${id}`)
+  }
+
+  const handleAddClick = () => {
+    router.push(`/friend/find`)
+  }
+
+  return (
+    <Stack>
+      <Stack direction="row" alignItems="center" justifyContent="space-between">
+        <Typography variant="title3" py={2} color="ink.dark">
+          My Friends
+        </Typography>
+        <Button size="small" onClick={() => handleAddClick()}>
+          <FiUserPlus style={{ marginRight: 8 }} /> Add
+        </Button>
+      </Stack>
+      <SearchBar />
+      <Stack p={2}>
+        {Friends
+          ? Friends.map((elem) => (
+              <div key={elem.username} onClick={() => handleFriendClick(elem.username)}>
+                <Stack direction="row" spacing={2}>
+                  <StatusBadge username={elem.username}>
+                    <Avatar
+                      src={elem.profilePictureURL}
+                      sx={{ bgcolor: 'primary.main', width: 40, height: 40 }}
+                    >
+                      {elem.displayName.charAt(0).toUpperCase()}
+                    </Avatar>
+                  </StatusBadge>
+                  <Stack spacing={1} justifyContent="center">
+                    <Typography variant="regular">{elem.displayName}</Typography>
+                    <Typography variant="tiny" color="sky.dark">
+                      {elem.username}
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </div>
+            ))
+          : null}
+      </Stack>
+    </Stack>
+  )
 }
+
+export default Index
