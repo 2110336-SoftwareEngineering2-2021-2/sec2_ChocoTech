b6c43cb4cecbf98a0775b992733f9538ccbde9b1
Pipat Saengow <pay2630@gmail.com>
2022-05-04 11:39:26 +0700

    Fix startup script

--- start_production_docker.sh
+++ start_production_docker.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 NODE_ENV=production node dist/apps/doji-backend/main.js &
 cd dist/apps/doji-frontend/
 ../../../node_modules/.bin/next start -p 3000 &
 echo "Started Doji Backend and Frontend"
 cd ../../../
+
+nginx
+
+wait -n
+pkill -P $$
